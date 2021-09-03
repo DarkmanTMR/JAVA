d54baa6807763154325adc5eec3b1633aebfb634
DarkmanTMR <trp1prt1@gmail.com>
2021-09-03 11:04:00 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,30 +10,56 @@ public class Main {
 
 
 
-
+//
         Deputy deputy = new Deputy("Volodya","Zelya", 35,false);
         Deputy deputy1 = new Deputy("Volodya","Zelya", 25,true,200);
         Deputy deputy2 = new Deputy("Volodya","Zelya", 31,false);
         Deputy deputy3 = new Deputy("Volodya","Zelya", 30,true,300);
-//        deputy.giveBribe(50);
-//
+////        deputy.giveBribe(50);
+////
         List<Deputy> strings= new ArrayList<>();
         strings.add(deputy1);
         strings.add(deputy2);
         strings.add(deputy3);
-
-//        System.out.println(strings);
-
+//
+////        System.out.println(strings);
+//
     Fraction fraction = new Fraction();
     fraction.addDeputy(deputy1);
     fraction.addDeputy(deputy2);
     fraction.addDeputy(deputy3);
+//
+//    System.out.println(fraction);
+////    fraction.delDeputy(deputy1);
+////        System.out.println(fraction);
+////        fraction.delAllDeputy();
+//        fraction.getAllDeputy();
+////        System.out.println(fraction);
 
-    System.out.println(fraction);
-//    fraction.delDeputy(deputy1);
-//        System.out.println(fraction);
-//        fraction.delAllDeputy();
-        fraction.getAllDeputy();
-//        System.out.println(fraction);
+//Map<Integer, Object> mapick = new HashMap<>();
+//mapick.put(1,deputy);
+//mapick.put(2,deputy1);
+//mapick.put(3,deputy2);
+//mapick.put(4,deputy3);
+//System.out.println(mapick);
+
+
+
+//System.out.println(parlament);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
--- src/Parlament.java
+++ src/Parlament.java
@@ -1,3 +1,61 @@
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class Parlament {
+    private Map<String, Fraction> fraction = new HashMap<>();
+
+    public Parlament() {
+    }
+
+    public Map<String, Fraction> getFraction() {
+        return fraction;
+    }
+
+    public void setFraction(Map<String, Fraction> fraction) {
+        this.fraction = fraction;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Parlament parlament = (Parlament) o;
+
+        return fraction.equals(parlament.fraction);
+    }
+
+    @Override
+    public String toString() {
+        return "Parlament{" +
+                "fraction=" + fraction +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+        return fraction.hashCode();
+    }
+
+    public void addFraction(){
+
+    }
+
+//todo клас Верховна рада
+//            поля
+//    мапа фракцій
+//    методи
+//    додати/видалити фракцію
+//    вивести всі фракції
+//    вивести конкретну фракцію
+//    додати/видалити депутата з фракції
+//    вивести всіх хабарників фракції
+//    вивести найбільшого хабарника у фрації
+//    вивести найбільшого хабарника верховної ради
+//    вивести фсіх депутатів фракції
+//    вивести найбільшого хабарника фракції
+//
+
 
 }
